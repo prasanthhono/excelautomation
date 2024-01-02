fd2bf2e577f3bcb7d75444ca60c5fcec9ffdc079
Prasanth Raghavulu <prasanth@hono.ai>
2024-01-03 00:22:44 +0800

    refactor slightly

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ function createWorkbook(metadata) {
   return workbook;
 }
 
+
 function addSheet(workbook, sheetData) {
   const worksheet = workbook.addWorksheet(sheetData.name, { properties: { tabColor: { argb: sheetData.color || 'FFFFFFFF' } } });
 
@@ -66,97 +67,53 @@ function addSheet(workbook, sheetData) {
 
     // Add details about the columns in rows 2 to 6
     sheetData.columns.forEach((column, index) => {
-      const rowIndex =  2;
+      const rowIndex = 2;
+      const columnLetter = worksheet.getColumn(index + 1).letter;
+
       // Header Row
-      worksheet.getCell(`${worksheet.getColumn(index + 1).letter}${rowIndex - 1}`).value = `${column.header + (column.required ? ' *' : '')}`;
-      // add fill color to the cell
-      worksheet.getCell(`${worksheet.getColumn(index + 1).letter}${rowIndex - 1}`).fill = {
-        type: 'pattern',
-        pattern: 'solid',
-        fgColor: { argb: 'FFC000' },
-      };
-      // make it bold and font as Arial and size as 10, add border to the cell, add alignment to the cell, add wrap text to the cell, add width to the cell from column.width
-      worksheet.getCell(`${worksheet.getColumn(index + 1).letter}${rowIndex - 1}`).font = {
-        bold: true,
-        name: 'Arial',
-        size: 10,
-      };
-      worksheet.getCell(`${worksheet.getColumn(index + 1).letter}${rowIndex - 1}`).border = {
-        top: { style: 'thin' },
-        left: { style: 'thin' },
-        bottom: { style: 'thin' },
-        right: { style: 'thin' },
-      };
-      worksheet.getCell(`${worksheet.getColumn(index + 1).letter}${rowIndex - 1}`).alignment = {
-        vertical: 'middle',
-        horizontal: 'center',
-      };
-      worksheet.getCell(`${worksheet.getColumn(index + 1).letter}${rowIndex - 1}`).wrapText = true;
+      const headerCell = worksheet.getCell(`${columnLetter}${rowIndex - 1}`);
+      headerCell.value = `${column.header}${column.required ? ' *' : ''}`;
+      headerCell.fill = { type: 'pattern', pattern: 'solid', fgColor: { argb: 'FFC000' } };
+      headerCell.font = { bold: true, name: 'Arial', size: 10 };
+      headerCell.border = { top: { style: 'thin' }, left: { style: 'thin' }, bottom: { style: 'thin' }, right: { style: 'thin' } };
+      headerCell.alignment = { vertical: 'middle', horizontal: 'center' };
+      headerCell.wrapText = true;
       worksheet.getColumn(index + 1).width = column.width;
 
-      worksheet.getCell(`${worksheet.getColumn(index + 1).letter}${rowIndex}`).value = `Data Type: ${column.dataType}`;
-      // add fill color to the cell
-      worksheet.getCell(`${worksheet.getColumn(index + 1).letter}${rowIndex}`).fill = {
-        type: 'pattern',
-        pattern: 'solid',
-        fgColor: { argb: 'D9D2E9' },
-      };
-      worksheet.getCell(`${worksheet.getColumn(index + 1).letter}${rowIndex + 1}`).value = `Required: ${column.required ? 'TRUE' : 'FALSE'}`;
-      // add fill color to the cell
-      worksheet.getCell(`${worksheet.getColumn(index + 1).letter}${rowIndex + 1}`).fill = {
-        type: 'pattern',
-        pattern: 'solid',
-        fgColor: { argb: 'FFF2CC' },
-      };
-      worksheet.getCell(`${worksheet.getColumn(index + 1).letter}${rowIndex + 2}`).value = `Max Length: ${column.maxLength || ''}`;
-      // add fill color to the cell
-      worksheet.getCell(`${worksheet.getColumn(index + 1).letter}${rowIndex + 2}`).fill = {
-        type: 'pattern',
-        pattern: 'solid',
-        fgColor: { argb: 'F4CCCC' },
-      };
-      worksheet.getCell(`${worksheet.getColumn(index + 1).letter}${rowIndex + 3}`).value = `Formula: ${column.formula || 'NA'}`;
-      // add fill color to the cell
-      worksheet.getCell(`${worksheet.getColumn(index + 1).letter}${rowIndex + 3}`).fill = {
-        type: 'pattern',
-        pattern: 'solid',
-        fgColor: { argb: 'C9DAF8' },
-      };
-      worksheet.getCell(`${worksheet.getColumn(index + 1).letter}${rowIndex + 4}`).value = `Operator: ${column.operator || 'NA'}`;
-      // add fill color to the cell
-      worksheet.getCell(`${worksheet.getColumn(index + 1).letter}${rowIndex + 4}`).fill = {
-        type: 'pattern',
-        pattern: 'solid',
-        fgColor: { argb: 'B6D7A8' },
-      };
+      // Details Rows
+      for (let i = 0; i < 5; i++) {
+        const cell = worksheet.getCell(`${columnLetter}${rowIndex + i}`);
+        cell.value = i === 0 ? `Data Type: ${column.dataType}` : i === 1 ? `Required: ${column.required ? 'TRUE' : 'FALSE'}` : i === 2 ? `Max Length: ${column.maxLength || ''}` : i === 3 ? `Formula: ${column.formula || 'NA'}` : `Operator: ${column.operator || 'NA'}`;
+        cell.fill = { type: 'pattern', pattern: 'solid', fgColor: i === 0 ? { argb: 'D9D2E9' } : i === 1 ? { argb: 'FFF2CC' } : i === 2 ? { argb: 'F4CCCC' } : i === 3 ? { argb: 'C9DAF8' } : { argb: 'B6D7A8' } };
 
-      // Lock the header row not to allow user to edit it
-      worksheet.getCell(`${worksheet.getColumn(index + 1).letter}1`).protection = {
-        locked: true,
-        hidden: true,
-      };
+        // Lock the details rows not to allow the user to edit them
+        cell.protection = { locked: true, hidden: true };
+      }
 
-      // Lock the details rows not to allow user to edit them
+      // Lock the header row not to allow the user to edit it
+      worksheet.getCell(`${columnLetter}1`).protection = { locked: true, hidden: true };
+
       for (let i = rowIndex; i <= rowIndex + 4; i++) {
-        worksheet.getCell(`${worksheet.getColumn(index + 1).letter}${i}`).protection = {
+        worksheet.getRow(i).protection = {
           locked: true,
         };
       }
 
       // Adding data validations based on column data type
-      const range = `${worksheet.getColumn(index + 1).letter}7:${worksheet.getColumn(index + 1).letter}1048`;
+      const range = `${columnLetter}7:${columnLetter}1048`;
 
       if (column.dataType === 'numeric') {
-        worksheet.dataValidations.add(range, {
+        worksheet.dataValidations.add(range,{
           type: 'whole',
           operator: 'between',
           formulae: [0, Math.pow(10, column.length) - 1],
           showErrorMessage: true,
           errorTitle: 'Invalid Data',
           error: `${column.header} should be between 0 and ${Math.pow(10, column.length) - 1} are allowed`,
+          sqref: range,
         });
       } else if (column.dataType === 'decimal') {
-        worksheet.dataValidations.add(range, {
+        worksheet.dataValidations.add(range,{
           type: 'decimal',
           operator: 'between',
           formulae: column.formula,
@@ -165,9 +122,10 @@ function addSheet(workbook, sheetData) {
           error: `${column.header} should be between ${column.formula[0]} and ${column.formula[1]}`,
           promptTitle: 'Decimal',
           prompt: `The value must be between ${column.formula[0]} and ${column.formula[1]}`,
+          sqref: range,
         });
       } else if (column.dataType === 'text') {
-        worksheet.dataValidations.add(range, {
+        worksheet.dataValidations.add(range,{
           type: 'textLength',
           operator: 'lessThan',
           showErrorMessage: true,
@@ -175,9 +133,10 @@ function addSheet(workbook, sheetData) {
           formulae: [column.maxLength || column.formula],
           errorTitle: 'Invalid Data',
           error: `${column.header} text length should be less than ${column.maxLength} characters`,
+          sqref: range,
         });
       } else if (column.dataType === 'date') {
-        worksheet.dataValidations.add(range, {
+        worksheet.dataValidations.add(range,{
           type: 'date',
           operator: column.operator || 'lessThan',
           showErrorMessage: true,
@@ -185,23 +144,26 @@ function addSheet(workbook, sheetData) {
           formulae: [new Date(column.formula[0], column.formula[1], column.formula[2])],
           errorTitle: 'Invalid Date',
           error: `${column.header} should be ${column.operator} ${new Date(column.formula[0], column.formula[1], column.formula[2])}`,
+          sqref: range,
         });
       } else if (column.dataType === 'email') {
-        worksheet.dataValidations.add(range, {
+        worksheet.dataValidations.add(range,{
           type: 'textLength',
           operator: 'lessThan',
           showErrorMessage: true,
           allowBlank: true,
-          formulae: [50]
+          formulae: [50],
+          sqref: range,
         });
       } else if (column.dataType === 'list') {
-        worksheet.dataValidations.add(range, {
+        worksheet.dataValidations.add(range,{
           type: 'list',
           allowBlank: true,
           formulae: [column.formula],
+          sqref: range,
         });
       } else if (column.dataType === 'phone') {
-        worksheet.dataValidations.add(range, {
+        worksheet.dataValidations.add(range,{
           type: 'textLength',
           operator: 'lessThan',
           showErrorMessage: true,
@@ -209,6 +171,7 @@ function addSheet(workbook, sheetData) {
           formulae: [column.maxLength || column.formula],
           errorTitle: 'Invalid Data',
           error: `${column.header} text length should be less than ${column.maxLength} characters`,
+          sqref: range,
         });
       }
     });
